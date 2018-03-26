7e06337b5216474a82726fe9708ded32784a6dfe
Žiga Krašovec <zkrasovec@gmail.com>
2018-03-26 12:00:47 +0200

    Added newsletter overlay
    Implemented newsletter overlay, added styles and frontend functionality

--- Vaja02/js/main.js
+++ Vaja02/js/main.js
@@ -1,15 +1,28 @@
-const ParallaxContent = (article, nav) => {
+const ParallaxContent = (article, nav, overlay) => {
   let windowOffset = window.scrollY
   let articleOffset = article.offsetTop
   if (articleOffset - windowOffset > 0) {
     nav.style = `transform: translateY(${windowOffset / 2}px)`
+  }
+  if (articleOffset - windowOffset < 0 - window.innerHeight / 2) {
+    if (!localStorage.getItem("raftingOverlayWasVisible")) {
+      overlay.classList.add('is-active')
+      localStorage.setItem("raftingOverlayWasVisible", true)
+      let closeBtn = overlay.querySelectorAll('.close-overlay')
+      closeBtn.forEach(btn => {
+        btn.addEventListener('click', e => {
+          overlay.classList.remove('is-active')
+        })
+      })
+    }
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const article = document.querySelector('main')
   const nav = document.getElementById('main-nav')
+  const overlay = document.getElementById('overlay')
   window.addEventListener('scroll', e => {
-    ParallaxContent(article, nav)
+    ParallaxContent(article, nav, overlay)
   })
 })
